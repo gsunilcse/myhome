59f1e34cf00c0b71e9d18d542446f6b73879e0fd
Suneel Kumar Gudupu <Suneel.Gudupu@ivycomptech.com>
2019-09-17 00:07:55 +0530

    cross origin issue

--- src/main/java/com/myhome/controller/MemberController.java
+++ src/main/java/com/myhome/controller/MemberController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.myhome.entity.MemberEntity;
 import com.myhome.exceptions.RecordNotFoundException;
 import com.myhome.services.MemberService;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class MemberController {
 	
